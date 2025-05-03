b5f0bbc3d81eef44518615c34269ff0397214642
Lili Gallego <lgallego@unal.edu.co>
2025-05-03 11:33:50 -0500

    inputandoutput

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -9,9 +9,12 @@ from homework.src._internals.write_count_words import write_count_words
 
 
 def main():
+    
+    input_folder = "data/input"
+    output_folder = "data/output"
 
     ## mover a la funcion "read_all_lines"
-    all_lines = read_all_lines()
+    all_lines = read_all_lines(input_folder)
 
     ## mover a "preprocess_lines"
     all_lines = preprocess_lines(all_lines)
@@ -32,7 +35,7 @@ def main():
     #                 counter[w] = counter.get(w, 0) + 1
 
     ##
-    write_count_words(counter)
+    write_count_words(counter,output_folder)
 
 if __name__ == "__main__":
     main()
